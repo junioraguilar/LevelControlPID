9af10b2d0cec580eeb60874674ab2b766169a411
junioraguilar <39425476+junioraguilar@users.noreply.github.com>
2019-11-21 17:50:24 -0300

    Create pid_level.ino

---
+++ pid_level.ino
@@ -0,0 +1,66 @@
+#include <Ultrasonic.h>
+
+#define pino_trigger 7
+#define pino_echo 4
+
+Ultrasonic ultrasonic(pino_trigger, pino_echo);
+
+int IN1 = 9;
+int velocidadeA = 10;
+float velocidade = 0;
+ 
+#define kp 5
+#define ki 0.01
+#define kd 2
+#define sp 75
+
+float acao_anterior = 0;
+
+float integral_error = 0;
+float erro_anterior = 0;
+void setup(){
+pinMode(IN1,OUTPUT);
+pinMode(velocidadeA,OUTPUT);
+Serial.begin(9600);
+
+
+}
+ 
+void loop(){
+
+
+float pv = distancia();
+pv = map(pv, 46.0, 8.0, 0.0, 100.0);
+Serial.print("         variavel:");
+int variable = map(pv, 0, 100, 0, 40);
+Serial.println(int(variable));
+float erro = sp - pv;
+integral_error += erro * 0.03;
+float D = (erro - erro_anterior)/0.03;
+
+float pi = (kp*erro) + (integral_error*ki*kp) + (D*kd);
+
+if (pi > 100){
+  pi = 100;
+}
+if (pi < 0){
+  pi = 0;
+}
+Serial.print(" PID:");
+Serial.print(int(pi));
+float velocity = map(pi, 0.0, 100.0, 0 ,255.0);
+
+digitalWrite(IN1,HIGH);
+analogWrite(velocidadeA, velocity); 
+erro_anterior = erro;
+}
+
+
+float distancia(){   
+  float cmMsec, inMsec;
+  double microsec = ultrasonic.timing();
+  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  delay(30);
+  return cmMsec;
+  
+}
